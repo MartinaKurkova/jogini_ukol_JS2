c39be659d7b11a7eca3f39dd98ead36373a32ef5
Martina Kurkova <martinanoha@seznam.cz>
2025-04-30 13:11:13 +0200

    Hotovo

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import { render } from '@czechitas/render';
+import { Header } from "../components/Header";
+import { Intro } from "../components/Intro";
+import { Pose } from "../components/Pose";
+import '../global.css';
+import './index.css';
+
+document.querySelector('#root').innerHTML = render(
+  <div id="app" className="app container">
+    <Header title="Jogíni"/>
+    <Intro heading="Vítejte mezi Jogíny" text="Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem. Postupně se seznámíte se základními principy jógy, jak přístupovat k sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu svoje tělo, pocity a emoce." />
+    <Pose image="img/yoga-pose.jpg" />
+  </div>
+);
