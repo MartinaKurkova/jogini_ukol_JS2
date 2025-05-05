b6957533481efc0b0f54fcde81214747643fdc7c
Martina Kurkova <martinanoha@seznam.cz>
2025-05-05 17:06:14 +0200

    přidání alt atributu

--- src/components/Pose/index.jsx
+++ src/components/Pose/index.jsx
@@ -2,6 +2,6 @@ import "./index.css";
 
 export const Pose = (props) => {
     return (
-        <img className="pose" src={props.image} />
+        <img className="pose" src={props.image} alt="yoga-pose"/>
     )
 }
